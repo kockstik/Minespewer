7a0109c317bfb4c9ad5c9966c1e00d2c5d544d96
Kockstik <kostyadi04@mail.ru>
2026-04-18 20:20:45 +0700

    Physical edges

--- Assets/Entity/Scripts/Movement.cs
+++ Assets/Entity/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,13 @@ public class Movement : MonoBehaviour
     private Vector3 moveDir = Vector3.zero;
     private Rigidbody rb;
 
+    //[SerializeField] private float mapSize = 300f;
+    //[SerializeField] private float playerSize = 8.5f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //mapSize -= playerSize / 2;
     }
 
     public void Move(Vector3 direction)
@@ -22,5 +27,25 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         rb.AddForce(moveDir * speed);
+
+        /*
+        if (transform.position.x > mapSize)
+        {
+            rb.linearVelocity = new Vector3(Mathf.Clamp(rb.linearVelocity.x, 0, -float.MinValue), 0, rb.linearVelocity.z);
+        }
+        else if (transform.position.x < -mapSize)
+        {
+            rb.linearVelocity = new Vector3(Mathf.Clamp(rb.linearVelocity.x, 0, float.MaxValue), 0, rb.linearVelocity.z);
+        }
+
+        if (transform.position.z > mapSize)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, Mathf.Clamp(rb.linearVelocity.z, 0, -float.MinValue));
+        }
+        else if (transform.position.z < -mapSize)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, Mathf.Clamp(rb.linearVelocity.z, 0, float.MaxValue));
+        }
+        */
     }
 }
