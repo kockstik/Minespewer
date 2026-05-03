45afcdd25b7129f94e8b2df390d58c8dda5f1a45
Kockstik <kostyadi04@mail.ru>
2026-05-03 16:01:42 +0700

    Add checkup entityes in SpawnPoint

--- Assets/Entity/Spavner/Scripts/SpawnPoint.cs
+++ Assets/Entity/Spavner/Scripts/SpawnPoint.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] private float spawnDelay = 2f;
-    private float lastSpavnTime = 0f;
+    [SerializeField] private float checkupDelay = 10f;
+    private float lastCheckup = 0f;
 
     private Queue<Minespewer> toSpawn = new Queue<Minespewer>();
     private bool isQueueProcessing = false;
@@ -31,33 +32,34 @@ public class SpawnPoint : MonoBehaviour
     private void spawn(Minespewer entity)
     {
         entity.transform.position = transform.position;
-        entity.Destroyed += Destroyed;
         entity.gameObject.SetActive(true);
+        Add(entity);
     }
 
     void Update()
     {
         Debug.Log($"SpawnPoint: {entityes.Count} entities, {toSpawn.Count} in queue");
-        if (entityes.Count > 0)
-            Debug.Log(entityes[0]);
+        if (Time.time - lastCheckup > checkupDelay && entityes.Count == 1)
+        {
+            lastCheckup = Time.time;
+            foreach (var entity in entityes)
+            {
+                if (!entity.gameObject.activeInHierarchy)
+                    Remove(entity);
+            }
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.transform.parent.TryGetComponent(out Minespewer entity))
-        {
-            entity.Destroyed += Destroyed;
-            entityes.Add(entity);
-        }
+            Add(entity);
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.transform.parent.TryGetComponent(out Minespewer entity))
-        {
-            entity.Destroyed -= Destroyed;
-            entityes.Remove(entity);
-        }
+            Remove(entity);
 
         if (canSpawn && toSpawn.Count > 0)
         {
@@ -69,8 +71,23 @@ public class SpawnPoint : MonoBehaviour
     void Destroyed(Entity entity)
     {
         if (entity is Minespewer mr)
-            entityes.Remove(mr);
+            Remove(mr);
+    }
+
+    private void Add(Minespewer entity)
+    {
+        if (entityes.Contains(entity))
+            return;
+        entity.Destroyed += Destroyed;
+        entityes.Add(entity);
+    }
+
+    private void Remove(Minespewer entity)
+    {
+        if (!entityes.Contains(entity))
+            return;
         entity.Destroyed -= Destroyed;
+        entityes.Remove(entity);
     }
 
     private IEnumerator ProcessSpawnQueue()
@@ -83,8 +100,7 @@ public class SpawnPoint : MonoBehaviour
             while (!canSpawn)
                 yield return null;
 
-            spawn(entity);
-            lastSpavnTime = Time.time;
+            spawn(entity); ;
             yield return new WaitForSeconds(spawnDelay);
         }
         isQueueProcessing = false;
